00e59ed8e161ba310f2402f37c10c5b276d88d64
1-aziat-1 <as.ac2727@mail.ru>
2021-09-04 01:22:30 +0300

    modernization

--- script/script.js
+++ script/script.js
@@ -8,14 +8,13 @@ let period= 3;
 let income = "фриланс";
 let num = 266219;
 let SumNum = 0;
-let sNum = num.toString().split('');
-let arr = sNum.map(Number);
-for (let i = 0; i < arr.length; i++) {
-    SumNum += arr[i];
-};
+let sNum = num.toString().split('').map(Number);
+sNum.forEach(function(elem){
+    SumNum += elem;
+    
+});
 SumNum **=3;
 console.log(SumNum.toString().substring(0,2));
-
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
