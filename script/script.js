5f34d71b86c8aad272f012d901cc160f0266ee71
1-aziat-1 <as.ac2727@mail.ru>
2021-09-15 04:01:24 +0300

    lesson09hard

--- script/script.js
+++ script/script.js
@@ -1,31 +1,39 @@
 "use strict";
 
-let money = 500;
-let addExpenses = "Интернет, такси, Комуналка";
-let deposit = true;
-let mission = 10000;
-let period= 3;
-let income = "фриланс";
-let num = 266219;
-let SumNum = 0;
-let sNum = num.toString().split('').map(Number);
-sNum.forEach(function(elem){
-    SumNum += elem;
-    
-});
-SumNum **=3;
-console.log(SumNum.toString().substring(0,2));
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+function myTyme(){
 
-console.log(addExpenses.length);
+let day = new Date();
 
-console.log("Период равен " + period+" месяцев");
-console.log("Цель заработать " + mission+" рублей");
 
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(`, `));
+let arrWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота","Воскресенье"];
 
-let budgetDay = money/30;
-console.log('budgetDay: ', budgetDay);
+let arrMonth = ["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"];
+
+const timeEnd = function(hour){
+     
+    let message = "";
+    hour == 1 ? message = " час " : hour == 2 || hour == 3 || hour == 4 ?  message = " часа "  : message = " часов ";
+    return message;
+};
+
+const pluseZero = function(chislo){
+
+    let message = "";
+
+    // chislo.toString.length == 1 ? message = "0" + chislo : message = chislo ; ПОЧЕМУ НЕ РАБОТАЕТ??
+
+    String(chislo).length == 1 ? message = "0" + chislo : message = chislo ; 
+
+    return message;
+}
+
+
+
+let str1 = "Сегодня " +  arrWeek[(day.getDay() + 6) % 7] + ", " + day.getDay() + " " +  arrMonth[day.getMonth()] + " " + day.getFullYear() + ", " + day.getHours() + timeEnd((day.getHours())) + day.getMinutes() + " минут " + day.getSeconds() + " секунды" ;
+
+let str2 = pluseZero(day.getDay()) + "." + pluseZero(day.getMonth())  + "." +  day.getFullYear() + " - " + pluseZero(day.getHours()) + ":" + pluseZero(day.getMinutes()) + ":" + pluseZero(day.getSeconds());
+
+document.querySelector('.time1').innerHTML = str1;
+document.querySelector('.time2').innerHTML = str2;
+}
+setInterval(myTyme);
